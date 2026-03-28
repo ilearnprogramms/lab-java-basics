e659dc38c777c900e86af3388de7a8e0242bef8a
Eren <erendogan13@gmail.com>
2026-03-28 09:59:55 +0100

    Solution to my excercise 1

--- ArrayMinMax.java
+++ ArrayMinMax.java
@@ -21,7 +21,7 @@ public class ArrayMinMax {
         for (int i = 0; i < numbers.length; i++) {
             System.out.print(numbers[i] + " ");
         }
-        System.out.println();
+        System.out.println("hello");
     }
 
     // sort them
